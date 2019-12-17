bb1869ed7cdd8c3e0c89507d0959d6cab90e0eb3
victortiads <baappaap123@hotmail.com>
2019-12-17 11:03:24 -0300

    bug fix

--- app/src/main/java/com/vtools/quicknews/view/activities/MainActivity.kt
+++ app/src/main/java/com/vtools/quicknews/view/activities/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.vtools.quicknews.view.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vtools.quicknews.R
 import com.vtools.quicknews.adapter.PageAdapter
-import com.vtools.quicknews.animation.FadeIn
-import com.vtools.quicknews.animation.FadeOut
-import com.vtools.quicknews.animation.HideToUp
-import com.vtools.quicknews.animation.ShowFromUpToDown
+import com.vtools.quicknews.animation.Animate
+import com.vtools.quicknews.animation.HideToLeft
+import com.vtools.quicknews.animation.ShowFromLeftToRight
 import com.vtools.quicknews.view.fragments.HomeFragment
 import com.vtools.quicknews.view.fragments.MenuFragment
 import com.vtools.quicknews.view.fragments.ProfileFragment
@@ -17,6 +18,7 @@ import com.vtools.quicknews.viewmodel.activities.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.navigation_drawer_layout.*
 import kotlinx.android.synthetic.main.toolbar_search_layout.*
+import org.jetbrains.anko.design.tabItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -45,22 +47,29 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = adapter
     }
 
-    private fun bottomNavigationChangeLister(){
+    private fun bottomNavigationChangeLister() {
         mBottomNavigationLayout.setOnNavigationItemSelectedListener {
-            when(it.itemId){
+
+            when (it.itemId) {
                 R.id.item_menu_home -> {
-                    mViewPager.currentItem = 0
-                    controlSeachbarView(0)
+                    if (!it.isChecked) {
+                        mViewPager.currentItem = 0
+                        controlSeachbarView(0)
+                    }
                     true
                 }
                 R.id.item_menu_menu -> {
-                    mViewPager.currentItem = 1
-                    controlSeachbarView(1)
+                    if (!it.isChecked) {
+                        mViewPager.currentItem = 1
+                        controlSeachbarView(1)
+                    }
                     true
                 }
                 R.id.item_menu_profile -> {
-                    mViewPager.currentItem = 2
-                    controlSeachbarView(2)
+                    if (!it.isChecked) {
+                        mViewPager.currentItem = 2
+                        controlSeachbarView(2)
+                    }
                     true
                 }
             }
@@ -68,14 +77,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun pagerChangeListener(){
-        mViewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+    private fun pagerChangeListener() {
+        mViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {}
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
             override fun onPageSelected(position: Int) {
-                when (position){
+
+                when (position) {
                     0 -> {
-                       mBottomNavigationLayout.selectedItemId = R.id.item_menu_home
+                        mBottomNavigationLayout.selectedItemId = R.id.item_menu_home
                         controlSeachbarView(position)
                     }
                     1 -> {
@@ -90,18 +106,17 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-    private fun controlSeachbarView(position:Int){
-        when(position){
+
+    private fun controlSeachbarView(position: Int) {
+        when (position) {
             0 -> {
-                seach_bar.ShowFromUpToDown()
+                seach_bar.ShowFromLeftToRight()
             }
-            1 -> {
-                seach_bar.HideToUp()
-            }
-            2 -> {
-                seach_bar.HideToUp()
+            else -> {
+                seach_bar.HideToLeft()
             }
         }
+
     }
 
 
